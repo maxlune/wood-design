358abadd886e96e5f2151b2ed16826aae02a39d6
LikaSpb <lobodzinskaya.lika@gmail.com>
2023-05-26 10:55:26 +0200

    update front

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,11 +4,11 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import img from "../assets/picture/img.png";
+import img from "../assets/picture/logo_maison.png";
 import AddFurnitureModal from "../components/AddFurnitureModal";
 
 const style = {
-  width: 50,
+  width: 100,
 };
 
 export default function ButtonAppBar() {
@@ -16,7 +16,7 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <img style={style} src={img} alt="Logo" />
+          <img style={style} src={img} alt="logo_maison" />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Wood Design
           </Typography>
